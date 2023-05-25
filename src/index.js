3c43aca675c5ccc292b6bd42added20c917d3e60
Yusuf Çolak <yusuf21706@gmail.com>
2023-05-25 16:20:29 +0300

    projeyi  redux toolkite çevirdim..

--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import { createStore } from "redux";
 import { Provider } from "react-redux";
-import { myReducer } from './reducers';
+import myReducer from "./store/reduxYapim";
+import { configureStore } from "@reduxjs/toolkit";
 
-const depo = createStore(myReducer);
-
+const store = configureStore({
+  reducer: myReducer,
+});
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={depo}>
+  <Provider store={store}>
     <BrowserRouter>
       <>
         <App />
@@ -21,7 +21,4 @@ root.render(
   </Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
---
+++ src/store/reduxYapim.js
@@ -0,0 +1,50 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  favs: [],
+  current: null,
+  error: null,
+  loading: true,
+};
+
+const mySlice = createSlice({
+  name: "myReducer",
+  initialState,
+  reducers: {
+    getFavsFromLocalStorage: (state) => {
+      // İşlemleri buraya ekleyin
+      return state;
+    },
+    addFav: (state, action) => {
+      // İşlemleri buraya ekleyin
+      return state;
+    },
+    removeFav: (state, action) => {
+      // İşlemleri buraya ekleyin
+      return state;
+    },
+    fetchSuccess: (state) => {
+      // İşlemleri buraya ekleyin
+      return state;
+    },
+    fetchLoading: (state) => {
+      // İşlemleri buraya ekleyin
+      return state;
+    },
+    fetchError: (state) => {
+      // İşlemleri buraya ekleyin
+      return state;
+    },
+  },
+});
+
+export const {
+  getFavsFromLocalStorage,
+  addFav,
+  removeFav,
+  fetchSuccess,
+  fetchLoading,
+  fetchError,
+} = mySlice.actions;
+
+export default mySlice.reducer;
